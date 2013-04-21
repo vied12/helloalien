b074acbd002f3b552bcdc113e5a4ef9cc52a3dbd
Edouard <edou4rd@gmail.com>
2013-04-21 13:08:03 +0200

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,14 @@ from fabric.contrib.console import confirm
 
 @hosts("sites@folky.fr")
 def deploy():
-	code_dir = '/home/sites/helloalien'
-	with cd(code_dir):
-		run("git pull")
-		run("venv/bin/python webapp.py collectstatic")
+	local('git checkout deployed')
+	local('git merge master')
+	local('rm static/.gitignore -f')
+	local('rm templates/.gitignore -f')
+	local('git add -u .')
+	local('git commit -m "updated"')
+	local('git push heroku deployed:master')
+	# code_dir = '/home/sites/helloalien'
+	# with cd(code_dir):
+	# 	run("git pull")
+	# 	run("venv/bin/python webapp.py collectstatic")
